8a93e31c83a4720e99ed1fd84949ef039e864e65
martinovicivica <ivica.martinovic@dominionenergy.com>
2022-04-28 15:34:27 -0600

    added db mongo

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -18,6 +19,11 @@ app.post("/name", (req, res) => {
   }
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`)
-})
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('Connected to database');
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  })
+});
+
